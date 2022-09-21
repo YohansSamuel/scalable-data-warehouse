877b89e17fce008eb856ee088085806e81ea8663
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-09-21 22:16:46 +0300

    add initial airflow dag

---
+++ dags/initial_airflow.py
@@ -0,0 +1,20 @@
+from functools import cache
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime
+
+with DAG(
+        dag_id='tutorial',
+        description='A simple tutorial DAG',
+        schedule_interval="@daily",
+        start_date=datetime(2022,1,1),
+        catcheup=False,
+        tags=['example'],
+    ) as dag:
+
+       @task(task_id="tutorial_task")
+       def print_helloworld():
+           print('Hellow Wordl')
+           return "Hello Wordld!"
+        
+print_helloworld()
